6b02b61586d41abb554d656061e126b3e2d05825
Athira Abdul Razak <athiraabdulrazak@gmail.com>
2021-09-27 22:03:30 -0700

    fix(contact form): contact form unused codes removed
    n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,46 +7,7 @@ import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  public myModel = ''
-  public mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
-  form: FormGroup;
-  submitted=false;
-  contactForm = this.fb.group({
-  contacts: this.fb.array([this.createContact()])
-  });
-
-constructor(private fb: FormBuilder) {}
- get contacts() {
-    return this.contactForm.get('contacts') as FormArray;
-  }
-
-  createContact(): FormGroup {
-    return this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: [''],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required,Validators.min(3)]],
-    });
-  }
-  addContacts() {
-    this.contacts.push(this.createContact());
-  }
-
-  deleteContact(i: number) {
-    this.contacts.removeAt(i);
-  }
-
-  get phone()
-  {
-    return this.contacts.get('phone');
-  }
-
-  onSubmit() {
-    this.submitted=true;
-    console.log(this.contactForm.value);
-  }
-}
+export class AppComponent {}
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TextMaskModule } from 'angular2-text-mask';
-
-
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormComponent } from './form/form.component';
-
+import { FormComponent } from './contactform/contactform.component';
+import { AddressformComponent } from './addressform/addressform.component';
+import { BiilingComponent } from './biiling/biiling.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
-  ],
+    FormComponent,
+    AddressformComponent,
+    BiilingComponent
+],
   imports: [
     BrowserModule,
     AppRoutingModule,
