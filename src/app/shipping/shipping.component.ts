2f156ca9f5655cc07306d51c8a391fed296fb92f
Athira Abdul Razak <athiraabdulrazak@gmail.com>
2021-10-07 02:59:49 -0700

    fix(shipping-form): unwanted codes removal

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
   styleUrls: ['./shipping.component.css']
 })
+
 export class ShippingComponent implements OnInit {
   shippingForm: FormGroup;
   submitted = false;
-
-  MethodList = [
+  methodList = [
     { code: 'UGTP', name: 'UPS Ground Third Party' },
     { code: 'UG', name: 'UPS Ground' }
   ];
@@ -27,31 +26,31 @@ export class ShippingComponent implements OnInit {
       'shipping_method': ['', Validators.required],
       'shipping_cost': ['', Validators.required],
       'shipping_account_number': ['', Validators.required],
-      'is_shipping_override': [false, Validators.required]
-
+      'is_shipping_override': [false,]
     });
   }
 
   onCheckboxChange(e: any) {
     if (e.target.checked) {
-      this.shippingForm.get('is_shipping_override').setValue(true);
       this.shippingForm.get('shipping_cost').disable();
     }
     else {
-      this.shippingForm.get('is_shipping_override').setValue(false);
       this.shippingForm.get('shipping_cost').enable();
     }
   }
 
   onSubmit() {
     this.submitted = true;
+    console.log(this.shippingForm.value);
+  }
+
+  onShippingMethodChange() {
     if (this.shippingForm.get('shipping_method').value === 'UGTP') {
       this.shippingForm.get('shipping_account_number').enable();
     }
     else {
       this.shippingForm.get('shipping_account_number').disable();
     }
-    console.log(this.shippingForm.value);
 
   }
 }
