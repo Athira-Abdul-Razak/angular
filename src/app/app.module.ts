e9f1babac67722d794f66226181d9622c2bf5d9b
Athira Abdul Razak <athiraabdulrazak@gmail.com>
2021-11-01 16:32:27 +0300

    feat(book-form): book-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ShippingComponent } from './shipping/shipping.component';
 import { ValidationComponent } from './shared/app-validation/app-validation.component';
 import { ValidationDirective } from './validation.directive';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { BookFormComponent } from './book/book-form/book-form.component';
+import { BookTableComponent } from './book/book-table/book-table.component';
 
 
 @NgModule({
@@ -25,6 +27,8 @@ import { LoginFormComponent } from './login-form/login-form.component';
     ValidationComponent,
     ValidationDirective,
     LoginFormComponent,
+    BookFormComponent,
+    BookTableComponent,
 
 ],
   imports: [
---
+++ src/app/book/book-form/book-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, Output, EventEmitter, Input, OnChanges } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-book-form',
+  templateUrl: './book-form.component.html',
+  styleUrls: ['./book-form.component.css']
+})
+
+export class BookFormComponent implements OnChanges {
+  @Output() updateBookForm = new EventEmitter<any>();
+  @Output() cancelEvent = new EventEmitter<any>();
+  @Output() updateEvent = new EventEmitter<any>();
+  @Input() row: any;
+  @Input() popupName: string;
+  submitted: boolean;
+  bookForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.bookForm = this.formBuilder.group({
+      'title': ['', [Validators.required]],
+      'author': ['', Validators.required],
+      'publisher': ['', Validators.required],
+      'genre': ['', Validators.required],
+      'price': ['', Validators.required]
+    });
+  }
+
+  ngOnChanges() {
+    console.log(this.popupName, 'HI');
+    if (this.row) {
+      this.bookForm.setValue(this.row);
+    }
+  }
+
+  onSubmit() {
+    if (this.bookForm.valid) {
+      this.updateBookForm.emit(this.bookForm.value);
+      this.bookForm.reset();
+
+    } else {
+      this.submitted = true;
+      this.bookForm.reset();
+    }
+  }
+
+  cancel() {
+    this.cancelEvent.emit(true);
+  }
+
+  update() {
+    this.updateEvent.emit(true);
+  }
+
+}
