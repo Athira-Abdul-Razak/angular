1a6ae4cd13a04e66e3eb8c3d58cce876b7dc4827
Athira Abdul Razak <athiraabdulrazak@gmail.com>
2021-11-03 10:42:07 +0300

    feat(routing-module): routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { RegisterFormComponent } from './address-form/register-form.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { BookTableComponent } from './book/book-table/book-table.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Contact-Form', component: ContactFormComponent },
+{ path: 'Register-Form', component: RegisterFormComponent },
+{ path: 'Shipping-Form', component: ShippingComponent },
+{ path: 'Login-Form', component: LoginFormComponent },
+{ path: 'Book-Table', component: BookTableComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
