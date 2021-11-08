85c940269f68ea7f5f6e0591d52ad6be7bd70981
Athira Abdul Razak <athiraabdulrazak@gmail.com>
2021-11-08 14:33:20 +0300

    perf(price-pipe): math.floor addition

--- src/app/pipes/price.pipe.ts
+++ src/app/pipes/price.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'price'
 })
-export class PricePipe implements PipeTransform {
-  transform(value: number): number {
-    var num1 = Math.round((value * 100) / 100);
-    return num1;
 
+export class PricePipe implements PipeTransform {
+  transform(value: number): any {
+    if (value % 0.5 === 0) {
+      var num = Math.floor((value * 100) / 100);
+      return num;
+    }
+    else {
+      var num = Math.round((value * 100) / 100);
+      return num;
+    }
   }
 }
